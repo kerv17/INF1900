825ece59b68fb39fe1d3671a0b4d6255c9c822d5
Sanmar Simon <sanmarsimon@gmail.com>
2020-04-11 23:20:56 -0400

    Détection SONAR fonctionnelle 100%

--- CodeCommun/Projet/test/sonar.cpp
+++ CodeCommun/Projet/test/sonar.cpp
@@ -8,28 +8,15 @@
 #include "../../Lib/lcm_so1602dtr_m_fw.h"
 
 
-/********************************* PRINCIPE DE FONCTIONNEMENTS DU CODE POUR LES CAPTEURS SONAR ********************************
- * 
- * Les broches qui receoivent les echo (PA0, PA1, PA2) sont mises en entrées. Ces broches sont reliées (au niveau hardware)
- * à PCINT0, PCINT1, PCINT2, VOIR FONCTIONNEMENT ATMEL. On lance le trigger, et ainsi la valeur sur les broches de l'echo passe 
- * directement à 1. Lorsque la valeur sur ces broches change de 0 à 1, alors il y'a une interruption. 
- * Dans cette interruption on lance le timer0. Lorsque la valeur sur ces broches passe de 1 à 0 (fin echo), alors il y'a une interruption.
- * Dans cette interruption on arrete le timer et on lis le temps écoulé
- * Ensuite on calcul la distance = TCNT0/58
-******************************************************************************************************************************/
-
 #define DEMO_DDR	DDRB // `Data Direction Register' AVR occup� par l'aff.
 #define DEMO_PORT	PORTB // Port AVR occup� par l'afficheur
 
 volatile double distance_gauche ;
+volatile double distance_devant;
+volatile double distance_droite;
 char affichageGauche[5];
 char affichageDevant[5];
 char affichageDroite[5];
-volatile double distance_devant;
-volatile double distance_droite;
-
-// The variable ‘pulse’ is used to store the count value from the TCNT register.
-static volatile double pulse = 0;
 
 // Ecran LCD
 LCM ecranLCD(&DEMO_DDR, &DEMO_PORT);
@@ -51,16 +38,15 @@ char* messageDistance(double distance)
     }
 }
 
+// Conversion double en string
 void conversion(double a,char* b )
 {
-uint8_t decimale =(uint8_t(a*10)%10);
-uint8_t entier = (uint8_t(a)%10);
+    uint8_t decimale =(uint8_t(a*10)%10);
+    uint8_t entier = (uint8_t(a)%10);
 
-b[0]='0'+entier;
-b[1]='.';
-b[2]='0'+decimale;
-
-
+    b[0]='0'+entier;
+    b[1]='.';
+    b[2]='0'+decimale;
 }
 
 void initialization() 
@@ -74,32 +60,10 @@ void initialization()
     PD7 et PD2(LED droite), PD0 et PD1(LED gauche) sont en sorties */
     DDRD = 0xf7;  
 
-    EIMSK |= (1 << INT1) | (1 << INT0);                   // Activer les interruptions externes sur INT1
-    EICRA |= (1 << ISC11) | (1 << ISC10) | (1 << ISC00);   // regler INT1 pour declencher au front montant uniquement et PCINT0 tous les fronts
+    EIMSK |= (1 << INT1);     // Activer les interruptions externes sur INT1
+    EICRA |= (1 << ISC11) | (1 << ISC10);   // regler INT1 pour declencher au front montant uniquement
 
-
-    // Active les interruptions sur tout changement logique de PA0-PA2
-    PCICR |= (1 << PCIE0);
-    PCMSK0 |= (1 << PCINT0) | (1 << PCINT1) | (1 << PCINT2) ;
     sei();				                    // sei permet de recevoir à nouveau des interruptions.
-}
-
-ISR(PCINT0_vect)
-{
-    if((PINA & 0x01) /*|| (PINA & 0x02) || (PINA & 0x04)*/)
-    {
-        // Start The TIMER prescale by 1024
-        TCCR0B |= (1 << CS00) | (1 << CS02);
-        TCNT0 = 0;
-    }
-    else
-    {
-        // Stop the timer and save the number of ticks in "pulse"
-        TCCR0B |= 0;
-        pulse = TCNT0;
-        TCNT0 = 0;
-    }
-    PCIFR |= (0 << PCIF0);
 }
 
 int main()
@@ -107,53 +71,95 @@ int main()
     initialization();
     while(1)
     {
-        // interruption autorisé que pour changement sur PINA0 (PCINT0)
-        PCMSK0 |= (1 << PCINT0) | (0 << PCINT1) | (0 << PCINT2) ;
+        /************************************************************************
+        ************************** LECTURE DU SONAR GAUCHE **********************
+        *************************************************************************/
+
+        int timeCounter = 0;
         PORTB |= (1 << PINB0) ;         // On trigger le sonar
         _delay_us(10);
         PORTB &= ~(1 << PINB0);
-        while(PINA & 0x01)
-        {
-            /* On attends l'echo et donc que PINA0 passe à 0*/
+
+        while(!(PINA & (1 << PA0))){
+            /* On attends le début de la période haute */
         }
-        distance_gauche = pulse/58.0;
-        //itoa(distance_gauche, affichageGauche, 10);
-        
-        
-        conversion(distance_gauche,affichageGauche);
-       
-        ecranLCD.write( affichageGauche, 0);
-       
-        ecranLCD[16] = messageDistance(distance_gauche);
 
-        // PCMSK0 |= (0 << PCINT0) | (1 << PCINT1) | (0 << PCINT2) ;
-        // PORTB |= (1 << PINB0) ;
-        // _delay_us(10);
-        // PORTB &= ~(1 << PINB0);
-        // while(PINA & 0x02)
-        // {       
-        //     /* On attends */
-        // }
-        // distance_devant = pulse/58;
-        // itoa(distance_devant, affichageDevant, 10);
-        // ecranLCD.write(affichageDevant, 6);
-        // ecranLCD[22] = (messageDistance(distance_devant));
+        while (PINA & (1 << PA0))
+        {
+            /* On attends la fin de la période haute. On compte le temps avant le retour de l'echo */
+            timeCounter++;
+            _delay_us(9);
+            if (timeCounter > 3600)                         // 3600 = 400cm * 9 (400 cm est la valeur maximale)
+            {
+                timeCounter = 0;
+                break;
+            }
+        }
+        distance_gauche = (timeCounter/10)/58.0;
+        conversion(distance_gauche, affichageGauche);
+        ecranLCD.write(affichageGauche, 0);
+        ecranLCD << "m";                                    // unité de distance en metre
+        ecranLCD[16] = messageDistance(distance_gauche);    // OK ATTN DNGR en fonction de la distance
 
-        // PCMSK0 |= (0 << PCINT0) | (0 << PCINT1) | (1 << PCINT2) ;
-        // PORTB |= (1 << PINB0) ;
-        // _delay_us(10);
-        // PORTB &= ~(1 << PINB0);
-        // while(PINA & 0x04)
-        // {
-        //     /* On attends */
-        // }
-        // distance_droite = pulse/58;
-        // itoa(distance_droite, affichageDroite, 10);
-        // ecranLCD.write(affichageDroite, 12);
-        // ecranLCD[28] = (messageDistance(distance_droite));
+        /*************************************************************************
+        ************************** LECTURE DU SONAR DE DEVANT ********************
+        **************************************************************************/
 
-    }
-    
+        timeCounter = 0;
+        PORTB |= (1 << PINB0) ;         // On trigger le sonar
+        _delay_us(10);
+        PORTB &= ~(1 << PINB0);
+
+        while(!(PINA & (1 << PA1))){
+            /* On attends le début de la période haute */
+        }
+        while (PINA & (1 << PA1))
+        {
+            /* On attends la fin de la période haute. On compte le temps avant le retour de l'echo */
+            timeCounter++;
+            _delay_us(9);
+            if (timeCounter > 3600)                           // 3600 = 400cm * 9 (400 cm est la valeur maximale)
+            {
+                timeCounter = 0;
+                break;
+            }
+        }
+        distance_devant = (timeCounter/10)/58.0;
+        conversion(distance_devant, affichageDevant);
+        ecranLCD.write(affichageDevant, 6);
+        ecranLCD << "m";                                      // unité de distance en metre              
+        ecranLCD[22] = (messageDistance(distance_devant));    // OK ATTN DNGR en fonction de la distance
+
+        /*************************************************************************
+        ************************** LECTURE DU SONAR DROIT ************************
+        **************************************************************************/
+
+        timeCounter = 0;
+        PORTB |= (1 << PINB0) ;         // On trigger le sonar
+        _delay_us(10);
+        PORTB &= ~(1 << PINB0);
+
+        while(!(PINA & (1 << PA2))){
+            /* On attends le début de la période haute */
+        }
+
+        while (PINA & (1 << PA2))
+        {
+            /* On attends la fin de la période haute. On compte le temps avant le retour de l'echo */
+            timeCounter++;
+            _delay_us(9);
+            if (timeCounter > 3600)                         // 3600 = 400cm * 9 (400 cm est la valeur maximale)
+            {
+                timeCounter = 0;
+                break;
+            }
+        }
+        distance_droite = (timeCounter/10)/58.0;
+        conversion(distance_droite, affichageDroite);
+        ecranLCD.write(affichageDroite, 11);
+        ecranLCD << "m";                                    //unité de distance en metre
+        ecranLCD[28] = (messageDistance(distance_droite));  // OK ATTN DNGR en fonction de la distance
+    }  
     return 0;
 }
 
